4f000a4edda93a26912b3e88fc52a0d8d002a70f
Nilo <nilo@MacBook-Air-de-Nilo.local>
2026-05-12 10:37:34 -0500

    chore: add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   site: 'https://exploreperu.com',
   vite: {
     plugins: [tailwindcss()]
